1dfbeef262ee0bf2b1e7f9ee2f3d432331dcf303
KGM <KGM@CASL6-10-138.kgisledu.com>
2024-05-18 09:01:14 +0530

    msg

---
+++ frontend/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import Admin from './Components/Admin';
+import User from './Components/User';
+
+function App() {
+  return (
+    <div className="App">
+      <Admin/>
+      <User/>
+
+    </div>
+  );
+}
+
+export default App;
